adaa5c2176e0088bf7b8f3544bf4c775e1cc7e98
Paula Catalan <162711475+pauCat13@users.noreply.github.com>
2025-03-23 18:31:02 +0000

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    resolve: {
+      fallback: {
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'), 
+        buffer: require.resolve('buffer/'),
+        vm: require.resolve('vm-browserify')
+      }
+    }
+  };
+  
